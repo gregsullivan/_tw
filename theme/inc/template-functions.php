02c5b99921964a0f6c0126d0d1b9ebad5e3026ca
Greg Sullivan <greg@swivelbase.com>
2021-04-15 15:39:05 -0700

    Stop adding unused body classes

--- theme/inc/template-functions.php
+++ theme/inc/template-functions.php
@@ -6,27 +6,6 @@
  */
 
 /**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- * @return array
- */
-function _tw_body_classes( $classes ) {
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
-	}
-
-	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
-	}
-
-	return $classes;
-}
-add_filter( 'body_class', '_tw_body_classes' );
-
-/**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
 function _tw_pingback_header() {
