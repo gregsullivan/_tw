434232d3fd0d8a7b2fbb3ccf12c3f9c42008a25a
Greg Sullivan <greg@swivelbase.com>
2022-01-11 17:06:07 -0800

    Update `tailwind.config.js` for Tailwind 3

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -3,33 +3,30 @@ module.exports = {
 		// Manage Tailwind Typography's configuration in a separate file.
 		require( './tailwind-typography.config.js' ),
 	],
-	mode: 'jit',
-	future: {},
-	purge: {
-		content: [
-			// Ensure changes to theme.json rebuild your CSS.
-			'./theme/theme.json',
-			'./theme/**/*.php',
-		],
-
+	content: [
+		// Ensure changes to theme.json and all PHP files rebuild your CSS.
+		'./theme/theme.json',
+		'./theme/**/*.php',
+	],
+	safelist: [
 		// Prevent editor-specific styles from being purged.
-		safelist: [
-			'editor-post-title__block',
-			'editor-post-title__input',
-		],
-	},
+		'editor-post-title__block',
+		'editor-post-title__input',
+	],
 	theme: {
 		// Extend the default theme.
 		extend: {
 
 		},
 	},
-	variants: {},
 	plugins: [
+		// Add Tailwind Typography.
+		require( '@tailwindcss/typography' ),
+
 		// Extract colors and widths from theme.json.
 		require( '@_tw/themejson' )( require( '../theme/theme.json' ) ),
 
-		// Uncomment below to add first-party Tailwind plugins.
+		// Uncomment below to add additional first-party Tailwind plugins.
 		// require( '@tailwindcss/aspect-ratio' ),
 		// require( '@tailwindcss/forms' ),
 		// require( '@tailwindcss/line-clamp' ),
