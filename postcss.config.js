92f5c5eabea0a32eb9a97f067dad7ea7d42d6ae2
Greg Sullivan <greg@swivelbase.com>
2022-01-11 17:04:42 -0800

    Update `package.json` - Remove Stylelint - Switch from @_tw/typography to @tailwindcss/typography - Update to Tailwind 3 - Add _TW_TARGET environment variable to build process to distinguish front-end builds from editor builds - Stop using separate configuration entry points for front-end and editor builds - Update `postcss.config.js` to use `postcss-simple-vars`

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+/* eslint-env node */
+
+const selectors = ( 'editor' === process.env._TW_TARGET ) ? {
+	'post-title-selector': '.editor-post-title__block .editor-post-title__input',
+	'post-content-selector': 'body .block-editor-block-list__layout',
+} : {
+	'post-title-selector': '.entry-title',
+	'post-content-selector': '.entry-content',
+};
+
 module.exports = {
 	plugins: [
 		require( 'postcss-import' ),
+		require( 'postcss-simple-vars' )( { variables: selectors } ),
 		require( 'tailwindcss' ),
 	],
 };
--- tailwind/tailwind-frontend.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	presets: [
-		require( './tailwind.config.js' ),
-	],
-	plugins: [
-		require( '@_tw/typography' )(),
-	],
-};
