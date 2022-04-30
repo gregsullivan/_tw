21375943cbcb12b8efaa5169b6b98d091ac61769
Greg Sullivan <greg@gregsullivan.com>
2022-04-29 21:30:30 -0700

    Remove Custom Header support

--- theme/functions.php
+++ theme/functions.php
@@ -156,11 +156,6 @@ function _tw_scripts() {
 add_action( 'wp_enqueue_scripts', '_tw_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
