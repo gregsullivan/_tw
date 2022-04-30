290d8f27833ad605765458b4f8bead42464a21ea
Greg Sullivan <greg@gregsullivan.com>
2022-04-29 21:22:58 -0700

    Remove Customizer support

--- theme/functions.php
+++ theme/functions.php
@@ -169,8 +169,3 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
