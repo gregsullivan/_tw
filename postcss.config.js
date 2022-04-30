a8da53c52e376cdf5a05e6c7ab5cec95a2baa478
Greg Sullivan <greg@gregsullivan.com>
2022-04-29 21:19:02 -0700

    Reduce selector specificity

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 /* eslint-env node */
 
 const selectors = ( 'editor' === process.env._TW_TARGET ) ? {
-	'post-title-selector': '.editor-post-title__block .editor-post-title__input',
-	'post-content-selector': 'body .block-editor-block-list__layout',
+	'post-title-selector': '.editor-post-title__input',
+	'post-content-selector': '.block-editor-block-list__layout',
 } : {
 	'post-title-selector': '.entry-title',
 	'post-content-selector': '.entry-content',
