ae9d8e66a3cf60b373135271e752a5949c50ffbc
Greg Sullivan <greg@gregsullivan.com>
2022-09-08 22:54:13 -0700

    Stop setting `content_width`

--- theme/functions.php
+++ theme/functions.php
@@ -87,18 +87,6 @@ endif;
 add_action( 'after_setup_theme', '_tw_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function _tw_content_width() {
-	$GLOBALS['content_width'] = apply_filters( '_tw_content_width', 640 );
-}
-add_action( 'after_setup_theme', '_tw_content_width', 0 );
-
-/**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
