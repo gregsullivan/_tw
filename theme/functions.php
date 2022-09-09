c9fbc0be7f16043ed17f3c0f7731f8114304f50b
Greg Sullivan <greg@gregsullivan.com>
2022-09-08 22:48:57 -0700

    Remove theme support for core custom background feature

--- theme/functions.php
+++ theme/functions.php
@@ -71,18 +71,6 @@ if ( ! function_exists( '_tw_setup' ) ) :
 			)
 		);
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'_tw_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
-
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
