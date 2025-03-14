1a8d25ea6abce3a4fa1466a15294392aaee50920
Greg Sullivan <greg@gregsullivan.com>
2025-03-13 23:05:24 -0700

    Add `postcss-url` to prevent `url()` rebasing

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,5 +2,29 @@ export default {
 	plugins: {
 		'@tailwindcss/postcss': {},
 		'postcss-nesting': {},
+
+		/**
+		 * This is a hopefully temporary workaround to prevent `url()` values
+		 * from being rebased to the document root for bundlers by
+		 * `@tailwindcss/postcss`. See:
+		 * https://github.com/tailwindlabs/tailwindcss/pull/16965
+		 */
+		'postcss-url': {
+			url: function (asset) {
+				const prefixesToStrip = [
+					'./tailwind/custom/components/',
+					'./tailwind/custom/',
+					'./tailwind/',
+				];
+
+				for (const prefix of prefixesToStrip) {
+					if (asset.url.startsWith(prefix)) {
+						return asset.url.replace(prefix, '');
+					}
+				}
+
+				return asset.url;
+			},
+		},
 	},
 };
