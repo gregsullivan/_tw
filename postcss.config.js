8ecb72a95eecd0ef075a3813aa4a7c7ded539201
Greg Sullivan <greg@gregsullivan.com>
2025-06-06 13:58:27 -0700

    Begin using `cssnano` `@tailwindcss/postcss`’s `optimize` option prevents `postcss-advanced-variables` from working, regardless of plugin order.
    Switching to `cssnano` resolves the problem. I also reordered the `postcss.config.js` file to better represent the order in which the plugins seem to be loading.

--- postcss.config.js
+++ postcss.config.js
@@ -1,40 +1,44 @@
 export default () => {
-	return {
-		plugins: {
-			'postcss-advanced-variables': {
-				variables: {
-					target: process.env._TW_TARGET || 'frontend',
-				},
+	const plugins = {
+		'postcss-import-ext-glob': {},
+		'@tailwindcss/postcss': {
+			optimize: false,
+		},
+		'postcss-advanced-variables': {
+			variables: {
+				target: process.env._TW_TARGET || 'frontend',
 			},
-			'postcss-import-ext-glob': {},
-			'@tailwindcss/postcss': {
-				optimize: process.env._TW_ENV === 'production',
-			},
-			'postcss-nesting': {},
+		},
+		'postcss-nesting': {},
 
-			/**
-			 * This is a hopefully temporary workaround to prevent `url()` values
-			 * from being rebased to the document root for bundlers by
-			 * `@tailwindcss/postcss`. See:
-			 * https://github.com/tailwindlabs/tailwindcss/pull/16965
-			 */
-			'postcss-url': {
-				url: function (asset) {
-					const prefixesToStrip = [
-						'./tailwind/custom/components/',
-						'./tailwind/custom/',
-						'./tailwind/',
-					];
+		/**
+		 * This is a hopefully temporary workaround to prevent `url()` values
+		 * from being rebased to the document root for bundlers by
+		 * `@tailwindcss/postcss`. See:
+		 * https://github.com/tailwindlabs/tailwindcss/pull/16965
+		 */
+		'postcss-url': {
+			url: function (asset) {
+				const prefixesToStrip = [
+					'./tailwind/custom/components/',
+					'./tailwind/custom/',
+					'./tailwind/',
+				];
 
-					for (const prefix of prefixesToStrip) {
-						if (asset.url.startsWith(prefix)) {
-							return asset.url.replace(prefix, '');
-						}
+				for (const prefix of prefixesToStrip) {
+					if (asset.url.startsWith(prefix)) {
+						return asset.url.replace(prefix, '');
 					}
+				}
 
-					return asset.url;
-				},
+				return asset.url;
 			},
 		},
 	};
+
+	if ('production' === process.env._TW_ENV) {
+		plugins.cssnano = {};
+	}
+
+	return { plugins };
 };
