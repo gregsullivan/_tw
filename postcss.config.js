3613814b52450f4820ba7120e55605280bddb812
Greg Sullivan <greg@gregsullivan.com>
2025-09-05 15:43:15 -0700

    Update npm dependencies and remove `postcss-url` We had been using `postcss-url` as a way of working around rebased asset URLs. `@tailwindcss/postcss` has since been updated with a `transformAssetUrls` option that allows us to disable rebasing and remove `postcss-url`.

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ export default () => {
 		'postcss-import-ext-glob': {},
 		'@tailwindcss/postcss': {
 			optimize: false,
+			transformAssetUrls: false,
 		},
 		'postcss-advanced-variables': {
 			variables: {
@@ -10,30 +11,6 @@ export default () => {
 			},
 		},
 		'postcss-nesting': {},
-
-		/**
-		 * This is a hopefully temporary workaround to prevent `url()` values
-		 * from being rebased to the document root for bundlers by
-		 * `@tailwindcss/postcss`. See:
-		 * https://github.com/tailwindlabs/tailwindcss/pull/16965
-		 */
-		'postcss-url': {
-			url: function (asset) {
-				const prefixesToStrip = [
-					'./tailwind/custom/components/',
-					'./tailwind/custom/',
-					'./tailwind/',
-				];
-
-				for (const prefix of prefixesToStrip) {
-					if (asset.url.startsWith(prefix)) {
-						return asset.url.replace(prefix, '');
-					}
-				}
-
-				return asset.url;
-			},
-		},
 	};
 
 	if ('production' === process.env._TW_ENV) {
