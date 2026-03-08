bebaf8e82a39f7c4baa6235f3382f62f0c40b77a
Greg Sullivan <greg@gregsullivan.com>
2026-03-07 23:03:05 -0800

    Add support for Tailwind class ordering in PHP templates We used to support this with Tailwind 3. This adds support for Tailwind 4.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,16 @@
+import { fileURLToPath } from 'node:url';
 import globals from 'globals';
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+
+import phpTemplates from 'eslint-plugin-php-templates';
+import angularTemplateParser from '@angular-eslint/template-parser';
+import tailwindcss from 'eslint-plugin-tailwindcss';
+
+const tailwindRulesConfig = tailwindcss.configs['flat/recommended'].find(
+	(config) => config.name === 'tailwindcss:rules'
+);
+const tailwindRules = tailwindRulesConfig?.rules || {};
 
 export default [
 	{
@@ -26,10 +36,32 @@ export default [
 		},
 	},
 	{
-		files: ['node_scripts/*.js', 'tailwind/*.js', 'postcss.config.js'],
+		files: ['node_scripts/*.js', 'eslint.config.js', 'postcss.config.js'],
 		languageOptions: {
 			globals: {
 				...globals.node,
+			},
+		},
+	},
+	{
+		files: ['**/*.php'],
+		languageOptions: {
+			parser: phpTemplates.suppressParserErrors(angularTemplateParser),
+		},
+		plugins: {
+			'php-templates': phpTemplates,
+			tailwindcss,
+		},
+		processor: 'php-templates/strip-php',
+		rules: {
+			...tailwindRules,
+			'tailwindcss/no-custom-classname': 'off',
+		},
+		settings: {
+			tailwindcss: {
+				config: fileURLToPath(
+					new URL('./tailwind-intellisense.css', import.meta.url)
+				),
 			},
 		},
 	},
